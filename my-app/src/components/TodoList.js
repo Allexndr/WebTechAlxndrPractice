5a457676903983ae439ce46acee30744ff2079d6
aleksandr <sanmemik2000@gmail.com>
2025-01-29 11:50:49 +0500

    step 7 Refactored TodoList: added dynamic props and important tasks

--- my-app/src/components/TodoList.js
+++ my-app/src/components/TodoList.js
@@ -2,12 +2,19 @@ import React from "react";
 import TodoListItem from './TodoListItem';
 
 const TodoList = () => {
-    const items = ["Install React", "Study React", "Use React", "Build React App"];
+    const items = [
+        { label: "Install React", important: false },
+        { label: "Study React", important: true },
+        { label: "Use React", important: false },
+        { label: "Build React App", important: true }
+    ];
 
     return (
         <ul>
             {items.map((item, index) => (
-                <li key={index}><TodoListItem label={item} /></li>
+                <li key={index}>
+                    <TodoListItem label={item.label} important={item.important} />
+                </li>
             ))}
         </ul>
     );
