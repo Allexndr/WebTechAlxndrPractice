36c3c349599f0c23c8c913c10185dd328e1b4f74
aleksandr <sanmemik2000@gmail.com>
2025-01-29 12:11:20 +0500

    Step 12: Added Bootstrap and CSS styling for TodoList

--- my-app/src/components/TodoListItem.js
+++ my-app/src/components/TodoListItem.js
@@ -4,7 +4,7 @@ const TodoListItem = ({ label, important = false }) => {
     const style = {
         color: important ? 'tomato' : 'black'
     };
-    return <span style={style}>{label}</span>;
+    return <span className="todo-list-item" style={style}>{label}</span>;
 };
 
 export default TodoListItem;
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -6,15 +6,15 @@ import TodoList from "./components/TodoList";
 import './index.css';
 
 const todoData = [
-    { label: "Install React", important: false },
-    { label: "Study React", important: true },
-    { label: "Use React", important: false },
-    { label: "Build React App", important: true }
+    { label: "Install React", important: false, id: 1 },
+    { label: "Study React", important: true, id: 2 },
+    { label: "Use React", important: false, id: 3 },
+    { label: "Build React App", important: true, id: 4 }
 ];
 
 const App = () => {
     return (
-        <div>
+        <div className="container">
             <AppHeader />
             <SearchPanel />
             <TodoList todos={todoData} />
