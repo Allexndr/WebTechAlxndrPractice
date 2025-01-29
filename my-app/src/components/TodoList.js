2129a92e959d743aa5838b4c629f04f0b27b9cc3
aleksandr <sanmemik2000@gmail.com>
2025-01-29 12:00:14 +0500

    Step 11 (fix): Fixed formatting in TodoList.js

--- my-app/src/components/TodoList.js
+++ my-app/src/components/TodoList.js
@@ -2,11 +2,14 @@ import React from "react";
 import TodoListItem from "./todo-list-item";
 
 const TodoList = ({ todos }) => {
-    const elements = todos.map((item, index) => (
-        <li key={index}>
-            <TodoListItem {...item} />
-        </li>
-    ));
+    const elements = todos.map((item) => {
+        const { id, ...itemProps } = item;
+        return (
+            <li key={item.id}>
+                <TodoListItem {...itemProps} />
+            </li>
+        );
+    });
 
     return <ul>{elements}</ul>;
 };
