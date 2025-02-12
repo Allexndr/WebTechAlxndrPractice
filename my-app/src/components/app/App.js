bd07487109f4b6a29823673258311399813bb4ee
aleksandr <sanmemik2000@gmail.com>
2025-02-12 11:35:18 +0500

    Step 25: Implemented deleteItem function in App.js

--- my-app/src/components/app/App.js
+++ my-app/src/components/app/App.js
@@ -1,28 +1,48 @@
-import React from "react";
-import AppHeader from "../app-header/AppHeader";
-import SearchPanel from "../search-panel/SearchPanel";
-import TodoList from "../todo-list/TodoList";
-import ItemStatusFilter from "../item-status-filter/ItemStatusFilter";
+import React, { Component } from 'react';
+import AppHeader from '../app-header/AppHeader';
+import SearchPanel from '../search-panel/SearchPanel';
+import TodoList from '../todo-list/TodoList';
+import ItemStatusFilter from '../item-status-filter/ItemStatusFilter';
+
 import './app.css';
 
-const App = () => {
-    const todoData = [
-        { label: 'Drink Coffee', important: false, id: 1 },
-        { label: 'Make Awesome App', important: true, id: 2 },
-        { label: 'Have a lunch', important: false, id: 3 }
-    ];
+export default class App extends Component {
+    state = {
+        todoData: [
+            { label: 'Drink Coffee', important: false, id: 1 },
+            { label: 'Make Awesome App', important: true, id: 2 },
+            { label: 'Have a lunch', important: false, id: 3 }
+        ]
+    };
 
-    return (
-        <div className="todo-app">
-            <AppHeader toDo={1} done={3} />
-            <div className="top-panel d-flex">
-                <SearchPanel />
-                <ItemStatusFilter />
+    deleteItem = (id) => {
+        this.setState(({ todoData }) => {
+            const idx = todoData.findIndex((element) => element.id === id);
+
+            // Разбиваем массив на две части: до удалённого элемента и после
+            const before = todoData.slice(0, idx);
+            const after = todoData.slice(idx + 1);
+
+            // Объединяем массив без удалённого элемента
+            const newArray = [...before, ...after];
+
+            return {
+                todoData: newArray
+            };
+        });
+    };
+
+    render() {
+        return (
+            <div className="todo-app">
+                <AppHeader toDo={1} done={3} />
+                <div className="top-panel d-flex">
+                    <SearchPanel />
+                    <ItemStatusFilter />
+                </div>
+
+                <TodoList todos={this.state.todoData} onDeleted={this.deleteItem} />
             </div>
-
-            <TodoList todos={todoData} onDeleted={(id) => console.log('del', id)} />
-        </div>
-    );
-};
-
-export default App;
+        );
+    }
+}
