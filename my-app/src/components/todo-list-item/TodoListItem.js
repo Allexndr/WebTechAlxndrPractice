b7f0eff5c0b4d7c6757e0b28afcd84fb5dfff40c
aleksandr <sanmemik2000@gmail.com>
2025-02-12 11:14:28 +0500

    Step 21: Added state management for done tasks in TodoListItem

--- my-app/src/components/todo-list-item/TodoListItem.js
+++ my-app/src/components/todo-list-item/TodoListItem.js
@@ -2,20 +2,37 @@ import React, { Component } from 'react';
 import './todo-list-item.css';
 
 export default class TodoListItem extends Component {
+    state = {
+        done: false // ✅ Теперь мы отслеживаем выполнение задачи
+    };
+
     onLabelClick = () => {
-        console.log(`Done: ${this.props.label}`);
+        this.setState(({ done }) => ({
+            done: !done // ✅ Переключаем значение `done` при клике
+        }));
     };
 
     render() {
         const { label, important = false } = this.props;
+        const { done } = this.state;
+
+        let classNames = 'todo-list-item';
+        if (done) {
+            classNames += ' done'; // ✅ Если `done: true`, добавляем класс `done`
+        }
+
         const style = {
             color: important ? 'steelblue' : 'black',
             fontWeight: important ? 'bold' : 'normal'
         };
 
         return (
-            <span className="todo-list-item">
-                <span className="todo-list-item-label" style={style} onClick={this.onLabelClick}>
+            <span className={classNames}>
+                <span
+                    className="todo-list-item-label"
+                    style={style}
+                    onClick={this.onLabelClick} // ✅ При клике изменяется `done`
+                >
                     {label}
                 </span>
                 <button type="button" className="btn btn-outline-success btn-sm float-right">
